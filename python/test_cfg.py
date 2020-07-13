a065956f9676d75f33b6e7c8387b4d85b23beb14
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-14 01:50:19 +0530

    Update test_cfg.py

--- python/test_cfg.py
+++ python/test_cfg.py
@@ -4,7 +4,7 @@ import FWCore.ParameterSet.VarParsing as VarParsing
 process = cms.Process("FramesProducer")
 
 #process.source = cms.Source("EmptySource")
-process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring("file:/afs/cern.ch/user/s/schaudha/public/CMSSW_10_6_8/src/demo/ZprimeToTT_M-2000_W-20_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_AODSIM_PUMoriond17.root")
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring("myoutput.root")#"file:/afs/cern.ch/user/s/schaudha/public/CMSSW_10_6_8/src/demo/ZprimeToTT_M-2000_W-20_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_AODSIM_PUMoriond17.root")
     , skipEvents = cms.untracked.uint32(0)#options.skipEvents
     )
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(2) )
@@ -14,7 +14,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(2) )
 process.stuff=cms.EDProducer("MyStuffProducer") 
 
 process.TFileService = cms.Service("TFileService",
-    fileName = cms.string("myoutput.root")#options.outputFile
+    fileName = cms.string("myoutput1.root")#options.outputFile
     )
 
 #make sure our module is called every event
