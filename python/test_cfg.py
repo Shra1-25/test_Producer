8ea1192293da913a43c9b95a953c1eb572808730
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-14 02:48:50 +0530

    Update test_cfg.py

--- python/test_cfg.py
+++ python/test_cfg.py
@@ -7,7 +7,7 @@ process = cms.Process("FramesProducer")
 process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring("file:/afs/cern.ch/user/s/schaudha/public/CMSSW_10_6_8/src/demo/DoublePhotonPt10To100_pythia8_ReAOD_PU2017_MINIAODSIM.root")
     , skipEvents = cms.untracked.uint32(0)#options.skipEvents
     )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(2) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 #load our module
 #module stuff = MyStuffProducer {}
