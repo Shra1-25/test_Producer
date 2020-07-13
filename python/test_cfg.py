82f1965aba9dc59c4d0bcd0cf0b5c5f98bac3376
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-14 00:37:22 +0530

    Update test_cfg.py

--- python/test_cfg.py
+++ python/test_cfg.py
@@ -5,13 +5,13 @@ process = cms.Process("FramesProducer")
 
 process.source = cms.source("EmptySource", maxEvents=cms.untracked.uint32(2))
 
-  #load our module
-   #module stuff = MyStuffProducer {}
-  process.stuff=cms.EDProducer("MyStuffProducer") 
+#load our module
+#module stuff = MyStuffProducer {}
+process.stuff=cms.EDProducer("MyStuffProducer") 
   
-   #make sure our module is called every event
-   process.p = cms.Path(process.stuff)
+#make sure our module is called every event
+process.p = cms.Path(process.stuff)
 
-    #just to see that something is happening
-   service = Tracer {}
+#just to see that something is happening
+service = Tracer {}
 }
