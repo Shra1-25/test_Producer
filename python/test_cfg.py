dfc69c104e0344039b50f036087ad4616d029f81
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-13 15:25:34 +0530

    Create test_cfg.py

---
+++ python/test_cfg.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
+
+process = cms.Process("FramesProducer")
+
+process TEST = {
+    //just a dummy source to give us some empty events
+   source = EmptySource {
+      untracked int32 maxEvents=2
+   }
+
+   //load our module
+   module stuff = MyStuffProducer {}
+
+   //make sure our module is called every event
+   path p = { stuff }
+
+    //just to see that something is happening
+   service = Tracer {}
+}
