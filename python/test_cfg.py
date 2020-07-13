7de9928aaf5542c12203dad1220286f5902759c4
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-14 00:26:48 +0530

    Update test_cfg.py

--- python/test_cfg.py
+++ python/test_cfg.py
@@ -3,18 +3,14 @@ import FWCore.ParameterSet.VarParsing as VarParsing
 
 process = cms.Process("FramesProducer")
 
-process.source = {
-   #just a dummy source to give us some empty events
-   source = EmptySource {
-      untracked int32 maxEvents=2
-   }
+process.source = {source = EmptySource {untracked int32 maxEvents=2}
 
-   //load our module
+  #load our module
    module stuff = MyStuffProducer {}
 
-   //make sure our module is called every event
+   #make sure our module is called every event
    path p = { stuff }
 
-    //just to see that something is happening
+    #just to see that something is happening
    service = Tracer {}
 }
