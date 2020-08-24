a1b924642ff983f765e38a036ceef870463febd8
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-25 00:20:19 +0530

    Update classes.h

--- plugins/classes.h
+++ plugins/classes.h
@@ -1,4 +1,4 @@
-#include "StuffProducer/MyStuff/interface/trial1.h"
+#include "StuffProducer/MyStuff/interface/trial2.h"
 //#include "Analysis/MyStuff/interface/MyOtherStuff.h"
 //#include "StuffProd/DataFormats/MyStuff/interface/SampleProd.h"
 #include <vector>
@@ -7,7 +7,7 @@
 
 namespace { namespace {
   //say which template classes should have dictionaries
-  edm::Wrapper<trial1> trial1_template;
-  edm::Wrapper<std::vector<trial1> > trial1_vec_template;
+  edm::Wrapper<trial2> trial2_template;
+  edm::Wrapper<std::vector<trial2> > trial2_vec_template;
   //edm::Wrapper<std::vector<MyOtherStuff> > dummy2;
 } }
